410aaf5494194d50d33ee005fb52f787b1c1e1fa
ysparrk <youngseo0703@gmail.com>
2024-06-09 14:05:43 +0900

    feat: init

---
+++ exception_study/src/test/java/commerce/exception_study/ExceptionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package commerce.exception_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
